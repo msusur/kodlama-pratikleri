37735a689136d3bbcb22818b6a0934f566db4f20
Berkay Nizam <berkay@birikiuc.net>
2018-09-23 15:48:11 +0300

    Update kelimeyi-tersten-yazma-problemi.js
    Alternatif yöntem eklendi.

--- cozumler/kelimeyi-tersten-yazma-problemi/javascript/kelimeyi-tersten-yazma-problemi.js
+++ cozumler/kelimeyi-tersten-yazma-problemi/javascript/kelimeyi-tersten-yazma-problemi.js
@@ -9,3 +9,14 @@ function reverse(str, index) {
 const test_str = "TEST STRING KODLAMA PRATİKLERİ";
 console.log( reverse(test_str , 0) );
 // çıktı : İRELKİTARP AMALDOK GNIRTS TSET
+
+
+/* ALTERNATİF YÖNTEM */
+
+function reverse2(str){
+  return str.split("").reverse().join("");
+}
+
+const test_str2 = "Bu bir reverse test";
+console.log(reverse2(test_str2));
+// çıktı: tset esrever rib ub

b9f4e37fce55e02cd7562a684a1916a3838ad65c
mstfEmk <mstfEmk@gmail.com>
2018-09-26 14:00:11 +0300

    Kelimeyi tersten yazma problemi Php versiyonu eklendi

---
+++ cozumler/kelimeyi-tersten-yazma-problemi/php/reverseString.php
@@ -0,0 +1,25 @@
+﻿<?php
+	
+	header('Content-Type: text/html; charset=utf-8');	
+	
+	$str = "TEST STRING KODLAMA PRATIKLERI";
+	$reversedStr = Reverse($str);
+	
+	echo $reversedStr; // IRELKITARP AMALDOK GNIRTS TSET
+	
+	function Reverse($str) {
+		
+		$len = strlen($str);
+		
+		if ($len == 1) { // Recursion Base Case
+			return $str;
+		} else {
+			
+			$len--;
+			$firstLetter = substr($str, 0, 1);
+			$remaining = substr($str, 1, $len);
+			return Reverse($remaining).$firstLetter;
+			
+		}
+	}
+?>

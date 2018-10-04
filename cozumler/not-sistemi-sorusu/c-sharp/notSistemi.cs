9f18f09ac4b38896b4a97024832d1a82e0c6cad1
Muhammed Hilmi Koca <mhkoca60@gmail.com>
2018-10-04 18:07:02 +0300

    Update notSistemi.cs

--- cozumler/not-sistemi-sorusu/c-sharp/notSistemi.cs
+++ cozumler/not-sistemi-sorusu/c-sharp/notSistemi.cs
@@ -2,7 +2,7 @@ public string Score(int numericScore)
 {
     if (numericScore >= 0 && numericScore <= 100)
     {
-        Dictionary<int, string> scores = new Dictionary<int, string> { { 30, "FF" }, { 50, "FD" }, { 60, "DD" }, { 65, "CC" }, { 70, "CB" }, { 80, "BB" }, { 90, "BA" }, { 100, "AA" } };
+        Dictionary<int, string> scores = new Dictionary<int, string> { { 30, "FF" }, { 50, "FD" }, { 60, "DD" }, { 65, "CC" }, { 70, "CB" }, { 80, "BB" }, { 90, "BA" }, { 101, "AA" } };
 
         foreach (var key in scores.Keys)
             if (numericScore < key)

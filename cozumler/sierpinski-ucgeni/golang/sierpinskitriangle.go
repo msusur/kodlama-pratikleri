7c15503124a555de4c59777971a547e6ab6f4402
Muhammed Alkan <dev.mhlkan@gmail.com>
2018-11-11 16:18:02 +0300

    Golang çözümü optimize edildi

--- cozumler/sierpinski-ucgeni/golang/sierpinskitriangle.go
+++ cozumler/sierpinski-ucgeni/golang/sierpinskitriangle.go
@@ -1,91 +1,26 @@
 package main
-
+ 
 import (
-	"image"
-	"image/color"
-	"image/png"
-	"os"
+    "fmt"
+    "strings"
+    "unicode/utf8"
 )
-
-var width = 4096
-var img = image.NewRGBA(image.Rect(0, 0, width, width))
-var col color.Color
-
-func drawTriangleRotate180(len int, _x int, _y int) {
-	for x := 0; x < len; x++ {
-		for y := _y; y < (x+_y/2)*2; y++ {
-			img.Set(x+_x, y, col)
-		}
-	}
-
-	for x := 0; x < len; x++ {
-		for y := _y; y < (_y+len*2)-x*2; y++ {
-			img.Set(x+_x+len, y, col)
-		}
-	}
-}
-
-func drawSierpinskisTriangle(len, x, y int) {
-	for {
-		len = len / 2
-		if len > 0 {
-			drawTriangleRotate180(len, x+len, y-len*2) // top
-			drawSierpinskisTriangle(len, x+len, y-len*2)
-
-			drawTriangleRotate180(len, x-len, y+len*2) // left
-			drawSierpinskisTriangle(len, x-len, y+len*2)
-
-			drawTriangleRotate180(len, x+len*3, y+len*2) // right
-			drawSierpinskisTriangle(len, x+len*3, y+len*2)
-		} else {
-			break
-		}
-	}
-
-	// calc formula
-	// top		=> len/2, x+len, y-len*2 => top => 100, 600+100, 400/2
-	// left		=> len/2, x-len, y+len*2 => 100, 600-100, 400+100*2
-	// right 	=> len/2, x+len*3, y+len*2 => 100, 600+100*3, 400+100*2
-}
-
+ 
+var order = 4
+var grain = "*"
+ 
 func main() {
-
-	col = color.White
-	for index := 0; index <= width; index++ {
-		for index1 := 0; index1 < width; index1++ {
-			img.Set(index, index1, col)
-		}
-	}
-
-	//step 1=> main triangle
-	col = color.Black
-	for x := width / 2; x > 0; x-- {
-		for y := 0; y < width; y++ {
-			img.Set(x, y-(x-width/2)*2, col)
-		}
-	}
-	for x := width / 2; x < width; x++ {
-		for y := 0; y < width; y++ {
-			img.Set(x, y-(width/2-x)*2, col)
-		}
-	}
-
-	col = color.White
-
-	len := width / 4
-	x := width / 4
-	y := width / 2
-
-	//step 2=> draw first reverse triangle
-	drawTriangleRotate180(len, x, y)
-
-	//step 3=> draw drawSierpinskisTriangle by step 2
-	drawSierpinskisTriangle(len, x, y)
-
-	f, err := os.Create("SierpinskisTriangle.png")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	png.Encode(f, img)
+    t := []string{grain + strings.Repeat(" ", utf8.RuneCountInString(grain))}
+    for ; order > 0; order-- {
+        sp := strings.Repeat(" ", utf8.RuneCountInString(t[0])/2)
+        top := make([]string, len(t))
+        for i, s := range t {
+            top[i] = sp + s + sp
+            t[i] += s
+        }
+        t = append(top, t...)
+    }
+    for _, r := range t {
+        fmt.Println(r)
+    }
 }
